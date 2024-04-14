a1ff38327f4a50c7ecd69114e3dc82259e44cdf9
Ajay Kumar Dhakar <ajaydhaker2002@kgpian.iitkgp.ac.in>
2024-04-14 22:27:18 +0530

    frontend improved

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
     return (
         <>
-            <h1 className='text-4xl font-bold text-blue-500'>About Page</h1>
+            <h1 className='text-center text-4xl font-bold text-blue-500'>About Page</h1>
         </>
     );
 };
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { Link,useLocation } from 'react-router-dom';
 import { cssstyles } from './data/csstemplate';
 
-
-
 function Navbar(){
     const location = useLocation();
     return(
